02216a56d46c275e8d5296f9d5c81b8dbb88b3e7
Jenny Liu <jennyliuye@gmail.com>
2021-09-30 12:03:00 -0700

    add some company routes

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require("express");
 const { NotFoundError } = require("./expressError");
 
 const app = express();
+const companyRoutes = require("./routes/companies");
 
 app.use(express.json());
 
+
+app.use("/companies", companyRoutes);
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
